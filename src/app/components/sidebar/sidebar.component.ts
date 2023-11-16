729651ac9747d195b99dd926b86b66ad17e0abda
Cristiano Leite <94697822+CristianosLeite@users.noreply.github.com>
2023-11-16 12:01:39 -0300

    Improved some responsiveness

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent {
-
+  @Input()
   isExpanded: boolean = true;
 
   constructor() { }
