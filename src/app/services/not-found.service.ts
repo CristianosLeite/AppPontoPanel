c139fe0f25eeaabfe3f2d1527ee5a10f2dde79fa
Cristiano Leite <94697822+CristianosLeite@users.noreply.github.com>
2023-11-26 14:47:20 -0300

    Update not-found.service.ts

--- src/app/services/not-found.service.ts
+++ src/app/services/not-found.service.ts
@@ -1,10 +1,15 @@
 import { Injectable, Output, EventEmitter } from '@angular/core';
 
+/**
+ * Serviço para emitir o evento de not found
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class NotFoundService {
+  /**
+   * Evento de not found
+   */
   @Output() notFound = new EventEmitter<string>();
 
-  constructor() { }
 }
